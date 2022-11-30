1f422d38529f482120f7f94289b1341105e454db
zhoubaiqiang <1121915087@qq.com>
2022-11-30 15:46:32 +0800

    添加登录路由

--- App.vue
+++ App.vue
@@ -3,6 +3,13 @@
 		onLaunch: function() {
 			console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
 			console.log('App Launch')
+			console.log('App Launch')
+			uni.navigateTo({
+				url: '/pages/login/login',
+				success: res => {},
+				fail: () => {},
+				complete: () => {}
+			})
 		},
 		onShow: function() {
 			console.log('App Show')
