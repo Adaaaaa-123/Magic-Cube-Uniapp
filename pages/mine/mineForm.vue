9406f60658706c4c3f8b4599344bd08489995941
Lcyame <lcyame@126.com>
2022-12-08 15:00:40 +0800

    新增用户详情和编辑资料页面

---
+++ pages/mine/mineForm.vue
@@ -0,0 +1,262 @@
+<template>
+	<view class="container">
+		<view class="formBlock">
+			<uni-forms class="formBox" ref="valiForm" :rules="rules" :modelValue="formData" label-width="80px">
+				<uni-forms-item label="人脸库" required name="face">
+					<uni-data-picker placeholder="请选择人脸库" popup-title="请选择人脸库" :localdata="faceData"
+						v-model="formData.face">
+					</uni-data-picker>
+				</uni-forms-item>
+				<uni-forms-item label="姓名" required name="name">
+					<uni-easyinput v-model="formData.name" placeholder="请输入姓名" />
+				</uni-forms-item>
+				<uni-forms-item label="人脸照片" required name="photo">
+					<u-upload previewFullImage :fileList="formData.photo" @delete="deletePic" @afterRead="afterRead"
+						:maxCount="1"></u-upload>
+				</uni-forms-item>
+
+				<uni-forms-item class="checkboxItem" label="性别" required name="sex">
+					<uni-data-checkbox v-model="formData.sex" :localdata="sexData" />
+				</uni-forms-item>
+				<uni-forms-item label="出生日期" name="birthday">
+					<uni-datetime-picker type="date" :clear-icon="false" v-model="formData.birthday" placeholder="请选择出生日期" />
+				</uni-forms-item>
+				<uni-forms-item label="证件号码" name="IDno">
+					<uni-easyinput v-model="formData.IDno" placeholder="证件号码" />
+				</uni-forms-item>
+			</uni-forms>
+		</view>
+		<view class="formBtn">
+			<button type="primary" @click="toVerify('valiForm')">提交</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	let that;
+	export default {
+		data() {
+			return {
+				formData: {
+					face: '',
+					name: '',
+					photo: [],
+					sex: 0,
+					birthday: "",
+					IDno: ""
+				},
+				base64: "",
+				//性别
+				sexData: [{
+					text: '男',
+					value: 0
+				}, {
+					text: '女',
+					value: 1
+				}, {
+					text: '保密',
+					value: 2
+				}],
+				//人脸库
+				faceData: [],
+				// 校验规则
+				rules: {
+					face: {
+						rules: [{
+							required: true,
+							errorMessage: '请选择人脸库'
+						}]
+					},
+					name: {
+						rules: [{
+							required: true,
+							errorMessage: '请输入姓名'
+						}]
+					},
+					photo: {
+						rules: [{
+							validateFunction: function(rule, value, data, callback) {
+								if (value.length < 1) {
+									callback('请上传人脸照片')
+								}
+								return true
+							}
+						}]
+					},
+					birthday: {
+						rules: [{
+							required: true,
+							errorMessage: '请选择出生日期'
+						}]
+					},
+					IDno: {
+						rules: [{
+							required: true,
+							errorMessage: '请输入证件号码'
+						}]
+					},
+				},
+			}
+		},
+		onLoad() {
+			that = this;
+			this.getFaceData()
+		},
+		methods: {
+			//验证
+			toVerify(ref) {
+				this.$refs[ref].validate().then(res => {
+					console.log('success', res);
+					console.log(this.formData);
+					this.toSubmit(res)
+				}).catch(err => {
+					console.log('err', err);
+				})
+			},
+			//提交
+			toSubmit(data) {
+				let params = {
+					imageDatas: [{
+						base64Binary: that.base64.replace(/^data:.*;base64,/, ''),
+						person: {
+							birthday: data.birthday,
+							genderCode: data.sex,
+							idNumber: data.IDno,
+							name: data.name
+						}
+					}],
+					libraryId: data.face
+				}
+
+				// this.$util.requestFunc(`/ftas/rest/face/library/${params.libraryId}/image/batch`, "POST", params, res => {
+				// 	if (res.data.responseStatus.statusCode == "0") {
+				// 		uni.showToast({
+				// 			title:res.data.responseStatus.message,
+				// 			icon:'none'
+				// 		})
+				// 		uni.navigateBack({
+				// 			delta: 1
+				// 		})
+				// 	} else {
+				// 		uni.showToast({
+				// 			title:'操作失败',
+				// 			icon:'none'
+				// 		})
+				// 	}
+				// },()=>{
+				// 	uni.showToast({
+				// 		title:'操作失败',
+				// 		icon:'none'
+				// 	})
+				// })
+
+				// this.$http.post(`/ftas/rest/face/library/${this.myFormData.face.dataId}/image/batch`, params).then(res => {
+				// 	if (res.data.responseStatus.statusCode == "0") {
+				// 		that.$tip.success(res.data.responseStatus.message);
+				// 		uni.navigateBack({
+				// 			delta: 1
+				// 		})
+				// 	} else {
+				// 		this.$tip.error('操作失败')
+				// 	}
+				// }).catch(() => {
+				// 	this.$tip.error('操作失败')
+				// });
+			},
+			//获取人脸库
+			getFaceData() {
+				// uni.showLoading({
+				// 	title: "加载中",
+				// 	mask: true
+				// });
+				// this.$util.requestFunc(`/mcms/rest/facelibrary/list`,"GET",{
+				// 	albumType: 2
+				// },res=>{
+				// 	uni.hideLoading();
+				// 	this.faceData = res.data.results
+				// },()=>{
+				// 	uni.hideLoading();
+				// 	uni.showToast({
+				// 		title:"获取人脸库失败",
+				// 		icon:'none'
+				// 	})
+				// })
+				this.faceData = [{
+					value: "1",
+					text: "1号库"
+				}, {
+					value: "2",
+					text: "2号库"
+				}, {
+					value: "3",
+					text: "3号库"
+				}, {
+					value: "4",
+					text: "4号库"
+				}, {
+					value: "5",
+					text: "5号库"
+				}, {
+					value: "6",
+					text: "6号库"
+				}]
+			},
+			// 删除图片
+			deletePic(event) {
+				// this[`fileList${event.name}`].splice(event.index, 1)
+				this.formData.photo = []
+			},
+			// 新增图片
+			async afterRead(event) {
+				console.log(JSON.stringify(event.file))
+				const result = await this.uploadFilePromise(event.file.url)
+				this.formData.photo = [event.file.url]
+			},
+			//上传图片
+			uploadFilePromise(path) {
+				let that = this;
+				return new Promise((resolve, reject) => {
+					// uni.uploadFile({
+					// 	url: configService.apiUrl + '/mcms/rest/distribution/getBase64Image',
+					// 	filePath: path,
+					// 	name: 'file',
+					// 	header: {
+					// 		'authorization': "Bearer " + uni.getStorageSync('authorization')
+					// 	},
+					// 	success: (res) => {
+					// 		that.base64 = res.data
+					// 	},
+					// 	fail: (res) => {
+					// 		this.$tip.error('文件转base64失败')
+					// 	}
+					// });
+					resolve(path)
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+	/deep/ .checkboxItem .uni-forms-item__content {
+		display: flex;
+		align-items: center;
+	}
+
+	/deep/ .u-upload__button {
+		margin: 0 !important;
+	}
+	
+	.container{
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	uni-page-body{
+		height: 100%;
+	}
+	.formBlock{
+		flex: 1;
+		overflow-y: auto;
+	}
+</style>
---
+++ pages/mine/mineInfo.vue
@@ -0,0 +1,128 @@
+<template>
+	<view class="container">
+		<uni-list>
+			<uni-list-item title="头像" class="avatarItem">
+				<template v-slot:footer>
+					<u-avatar :src="personalMsg.avatar"></u-avatar>
+				</template>
+			</uni-list-item>
+			<uni-list-item title="姓名" :rightText="personalMsg.realname" />
+			<uni-list-item title="性别" :rightText="personalMsg.sex==2?'男':'女'" />
+			<uni-list-item title="生日" :rightText="personalMsg.birthday" />
+			<uni-list-item title="对外信息展示" :rightText="personalMsg.realname+'@'+personalMsg.orgCode" />
+			<uni-list-item title="所在部门" :rightText="personalMsg.orgCode" />
+			<uni-list-item title="工号" :rightText="personalMsg.workNo" />
+			<uni-list-item title="状态" :rightText="personalMsg.status" />
+			<uni-list-item title="手机" :rightText="personalMsg.phone" />
+			<uni-list-item title="邮箱" :rightText="personalMsg.email" />
+			<uni-list-item title="职务" :rightText="personalMsg.post" />
+			<uni-list-item title="身份" :rightText="personalMsg.identity" />
+			<uni-list-item title="负责部门"  v-if="personalMsg.identity =='上级'" :rightText="personalMsg.departIds" />
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				personalMsg:{
+					// avatar: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg',
+					avatar: '',
+					realname: '张三',
+					username: '',
+					sex: 1,
+					birthday: "1986-01-11",
+					orgCode: '行政',
+					workNo: '001',
+					status: "1",
+					phone: '13888888888',
+					telephone: '13888888888',
+					email: '13888888888@126.com',
+					post: '职员',
+					departIds: '',
+					identity: '职员',
+				},
+				userUrl:'/sys/user/queryById',
+				positionUrl:'/sys/position/list',
+				departUrl:'/sys/user/userDepartList'
+			}
+		},
+		onNavigationBarButtonTap() {
+			console.log(1111)
+			uni.navigateTo({
+				url:"/pages/mine/mineForm"
+			})
+		},
+		onLoad() {
+			this.getInfo()
+		},
+		methods:{
+			getInfo(){
+				// this.$util.requestFunc(this.userUrl,"GET",{
+				// },res=>{
+				// 	uni.hideLoading();
+				// 	this.faceData = res.data.results
+				// },()=>{
+				// 	uni.hideLoading();
+				// 	uni.showToast({
+				// 		title:"获取人脸库失败",
+				// 		icon:'none'
+				// 	})
+				// })
+				// this.$http.get(this.userUrl,{params:{id:this.$store.getters.userid}}).then(res=> {
+				// 	console.log("用户",res)
+				// 	if (res.data.success) {
+				// 		let result = res.data.result
+				// 		if(result.avatar&&result.avatar.length >0)
+				// 		this.personalMsg.avatar = api.getFileAccessHttpUrl(result.avatar)
+				// 		this.personalMsg.realname = result.realname
+				// 		this.personalMsg.username= result.username
+				// 		this.personalMsg.post = result.post
+				// 		this.personalMsg.sex = result.sex===1?'男':'女'
+				// 		this.personalMsg.birthday = result.birthday== null?'无':result.birthday
+				// 		this.personalMsg.departIds= result.departIds
+				// 		this.personalMsg.workNo= result.workNo
+				// 		this.personalMsg.phone= result.phone
+				// 		this.personalMsg.telephone= result.telephone== null?'无':result.telephone
+				// 		this.personalMsg.email= result.email
+				// 		this.personalMsg.post= result.post
+				// 		this.personalMsg.identity= result.identity=== 1?'普通成员':'上级'
+				// 		this.personalMsg.status= result.status === 1?'正常':'冻结'
+				// 		this.personalMsg.orgCode= result.orgCode
+				// 	}
+				// }).catch(e=>{
+				// 	console.log("请求错误",e)
+				// })
+				
+				// this.$http.get(this.departUrl,{params:{userId:this.$store.getters.userid}}).then(res=> {
+				// 	if (res.success) {
+				// 		for (let item of res.result){
+				// 			this.personalMsg.orgCode = item.title
+				// 			this.personalMsg.departIds = item.title
+				// 		}
+				// 	}
+				// }).catch(e=>{
+				// 	console.log("请求错误",e)
+				// })
+				
+				// this.$http.get(this.positionUrl).then(res=> {
+				// 	if (res.success) {
+				// 		let postArr = res.result.records
+				// 		for (let item of postArr ){
+				// 			if (this.personalMsg.post == item.code){
+				// 				this.personalMsg.post = item.name
+				// 			}
+				// 		}
+				// 	}
+				// }).catch(e=>{
+				// 	console.log("请求错误",e)
+				// })
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("mine.css");
+</style>
